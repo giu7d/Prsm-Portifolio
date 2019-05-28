79b1af1a5d6ff1393bb9166a7f368bdb8537877b
Giuseppe Setem <giuseppe7d@gmail.com>
2019-05-28 08:59:50 -0300

    In Construction

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
@@ -9,16 +8,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Pagina em Construção <br />
+            <code className="App-link">console.log(404);</code>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
